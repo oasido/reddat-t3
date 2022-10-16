9b9dd06991c591d16f7c16ea79d06af7da51a871
Ofek Asido <asido.ofek@gmail.com>
2022-10-16 18:56:23 +0300

    feat: add post upvote/downvote ability

--- src/components/card/votes-component.tsx
+++ src/components/card/votes-component.tsx
@@ -1,20 +1,59 @@
 import { BiUpvote, BiDownvote } from "react-icons/bi";
+import { MouseEventHandler, useEffect, useState } from "react";
+import { trpc } from "../../utils/trpc";
+import { useSession } from "next-auth/react";
 
 type VotesProps = {
-  votes: number;
+  // userId: string;
+  postId: string;
+  /** 1 if the user has upvoted this post, -1 if they've downvoted, else 0. */
+  userMagnitude: -1 | 0 | 1;
+  /** Number of (upvotes - downvotes) this Post has received so far. */
+  votesCount: number;
 };
+export const VotesComponent = (props: VotesProps): JSX.Element => {
+  const { postId, userMagnitude, votesCount } = props;
 
-export const VotesComponent = ({ votes }: VotesProps): JSX.Element => {
+  const { data: sessionData } = useSession();
+
+  const voteOnPost = trpc.posts.vote.useMutation();
+
+  const [magnitude, setMagnitude] = useState(userMagnitude);
+
+  const makeHandleClick =
+    (newMagnitude: -1 | 0 | 1): MouseEventHandler =>
+    async (event) => {
+      event.preventDefault();
+
+      setMagnitude(newMagnitude);
+      console.log(votesCount - (magnitude - userMagnitude));
+
+      await voteOnPost.mutateAsync({
+        postId,
+        magnitude: newMagnitude,
+      });
+    };
+
+  useEffect(() => {
+    setMagnitude(userMagnitude);
+  }, [userMagnitude]);
+
   return (
     <div className="col-span-1 flex flex-col items-center justify-center bg-neutral-900/25 p-2 text-2xl ">
       <button className="radius rounded-full p-1.5 hover:bg-gray-200/10">
-        <BiUpvote className="text-neutral-500" />
+        <BiUpvote
+          onClick={makeHandleClick(magnitude > 0 ? 0 : 1)}
+          className={magnitude > 0 ? "text-red-500" : "text-neutral-500"}
+        />
       </button>
       <span className="text-center text-base font-[600] text-gray-200">
-        {votes}
+        {votesCount + (magnitude - userMagnitude)}
       </span>
       <button className="radius rounded-full p-1.5 hover:bg-gray-200/10">
-        <BiDownvote className="text-neutral-500" />
+        <BiDownvote
+          onClick={makeHandleClick(magnitude < 0 ? 0 : -1)}
+          className={magnitude < 0 ? "text-red-500" : "text-neutral-500"}
+        />
       </button>
     </div>
   );
---
+++ src/server/trpc/router/subrouters/posts.ts
@@ -0,0 +1,102 @@
+import { router, publicProcedure, protectedProcedure } from "../../trpc";
+import { z } from "zod";
+
+export const postsRouter = router({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany({
+      include: {
+        subreddit: true,
+        author: true,
+        PostVote: true,
+      },
+    });
+  }),
+
+  new: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.post.create({
+          data: {
+            title: input.title,
+            content: input.content,
+            subredditId: "cl9bfmp0k0000utl9nnh9fq1y",
+            authorId: ctx.session.user.id,
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+
+  vote: protectedProcedure
+    .input(
+      z.object({
+        postId: z.string(),
+        magnitude: z.number().gte(-1).lte(1).int(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId =
+        ctx.session.user.id ?? new Error("Authentication failed, try again");
+      const { postId, magnitude } = input;
+      try {
+        // First, handle the removal of any current votes.
+        const maybeVote = await ctx.prisma.postVote.findUnique({
+          where: { userId_postId: { userId, postId } },
+        });
+
+        if (maybeVote) {
+          // There was an old vote. Remove it!
+          const oldMagnitude = maybeVote.magnitude;
+          await ctx.prisma.$transaction([
+            ctx.prisma.postVote.delete({
+              where: { userId_postId: { userId, postId } },
+            }),
+            ctx.prisma.post.update({
+              where: { id: postId },
+              data: {
+                votesCount: { decrement: oldMagnitude },
+                [oldMagnitude >= 0 ? "upvotesCount" : "downvotesCount"]: {
+                  decrement: 1,
+                },
+              },
+            }),
+          ]);
+
+          // User had an old vote, so just remove their vote and return.
+          if (oldMagnitude === magnitude) {
+            return;
+          }
+        }
+
+        // If the user wants to remove their vote without any new vote, do that.
+        if (!magnitude) {
+          return;
+        }
+
+        // Add the new vote
+        await ctx.prisma.$transaction([
+          ctx.prisma.postVote.create({
+            data: { userId, postId, magnitude },
+          }),
+          ctx.prisma.post.update({
+            where: { id: postId },
+            data: {
+              votesCount: { increment: magnitude },
+              [magnitude >= 0 ? "upvotesCount" : "downvotesCount"]: {
+                increment: 1,
+              },
+            },
+          }),
+        ]);
+      } catch (error) {
+        console.error(error);
+      }
+    }),
+});
