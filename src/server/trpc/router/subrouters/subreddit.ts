7713a43904de05e02725f6cc93899cd547d66d4f
Ofek Asido <asido.ofek@gmail.com>
2022-10-21 19:15:15 +0300

    feat: include count in query

--- src/server/trpc/router/subrouters/subreddit.ts
+++ src/server/trpc/router/subrouters/subreddit.ts
@@ -15,6 +15,11 @@ export const subredditRouter = router({
         },
         include: {
           moderators: true,
+          _count: {
+            select: {
+              users: true,
+            },
+          },
         },
       });
     }),
