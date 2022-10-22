bcb65749655b80429a52e3377b33489eb14f94ff
Ofek Asido <asido.ofek@gmail.com>
2022-10-22 20:33:51 +0300

    chore: add github link with icon

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,6 +2,7 @@ import { Select } from "./select";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { UserMenu } from "./user-dropdown";
+import { FiGithub } from "react-icons/fi";
 
 export const Navbar = () => {
   const { data: sessionData } = useSession();
@@ -17,7 +18,15 @@ export const Navbar = () => {
         <Select />
       </div>
 
-      <div>
+      <div className="flex items-center justify-center">
+        <a
+          href="https://github.com/oasido/reddat-t3"
+          target="_blank"
+          rel="noreferrer"
+          className="mr-1 rounded-full p-2 text-xl text-gray-200 hover:bg-neutral-500/50"
+        >
+          <FiGithub />
+        </a>
         {sessionData ? (
           <UserMenu />
         ) : (
