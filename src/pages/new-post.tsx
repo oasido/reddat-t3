4d62d911cbf3f49467193a45e94d99bd2de24f64
oasido <asido.ofek@gmail.com>
2022-10-27 09:30:28 +0300

    chore: add new post button

--- src/pages/new-post.tsx
+++ src/pages/new-post.tsx
@@ -57,6 +57,12 @@ const NewPost: NextPage = () => {
           placeholder="Text (required)"
           className="h-36 w-full rounded-md border-neutral-700 bg-neutral-800 p-2 text-gray-200"
         ></textarea>
+        <div className="align-items flex justify-end">
+          Test
+          <button className="my-2 rounded-xl border-2 bg-gray-300 px-3 py-0.5 text-sm font-[600] hover:bg-gray-100">
+            Submit New Post
+          </button>
+        </div>
       </Container>
     </>
   );
