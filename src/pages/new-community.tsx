992cdab9d403d33651df4f2bf404a1140f06743f
oasido <asido.ofek@gmail.com>
2022-11-11 01:58:05 +0200

    feat: new-community page now works

--- src/pages/new-community.tsx
+++ src/pages/new-community.tsx
@@ -2,62 +2,59 @@ import Head from "next/head";
 import { NextPage } from "next";
 import { Container } from "../components/container";
 import { Navbar } from "../components/navbar";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { z } from "zod";
 import { trpc } from "../utils/trpc";
 import { useRouter } from "next/router";
-// import { useSession } from "next-auth/react";
+/* import { useSession } from "next-auth/react"; */
 
 const newCommunitySchema = z.object({
   name: z.string().min(3).max(20),
   description: z.string().min(3).max(100),
+  avatar: z.string().url().optional(),
+  image: z.string().url().optional(),
 });
 
-export type NewPostErrors = {
+export type newCommunityErrors = {
   name?: string[];
   description?: string[];
 };
 
 const NewCommunity: NextPage = () => {
-  // const { data: sessionData } = useSession();
+  /* const { data: sessionData } = useSession(); */
   const router = useRouter();
 
-  const [errors, setErrors] = useState<NewPostErrors>();
+  const [errors, setErrors] = useState<newCommunityErrors>();
 
-  const [post, setPost] = useState({
+  const [community, setCommunity] = useState({
     name: "",
     description: "",
+    image: "",
+    avatar: "",
   });
 
-  const newPost = trpc.posts.newPost.useMutation();
+  const newCommunity = trpc.subreddit.new.useMutation();
 
-  const handleNewCommunityButton = async ({
-    name,
-    description,
-  }: z.infer<typeof newCommunitySchema>) => {
-    const parsedNewCommunity = newCommunitySchema.safeParse({
-      name,
-      description,
-    });
-
-    // if (parsedNewCommunity.success) {
-    //   setErrors(undefined);
-    //   await newPost.mutateAsync(
-    //     {
-    //       name,
-    //       description,
-    //     },
-    //     {
-    //       onSuccess: () => {
-    //         setPost({ name: "", description: "" });
-    //         router.push("/");
-    //       },
-    //       onError: (error) => console.log(error),
-    //     }
-    //   );
-    // } else {
-    //   setErrors(parsedNewCommunity.error.flatten().fieldErrors);
-    // }
+  const handleNewCommunityButton = async () => {
+    const parsedCommunity = newCommunitySchema.safeParse(community);
+    if (parsedCommunity.success) {
+      const response = await newCommunity.mutateAsync(
+        {
+          name: parsedCommunity.data.name,
+          description: parsedCommunity.data.description,
+          image: parsedCommunity.data.image,
+        },
+        {
+          onSuccess: () => {
+            setCommunity({ name: "", description: "", image: "", avatar: "" });
+            router.push("/");
+          },
+          onError: (error) => console.log(error),
+        }
+      );
+    } else {
+      setErrors(parsedCommunity.error.flatten().fieldErrors);
+    }
   };
 
   return (
@@ -75,15 +72,44 @@ const NewCommunity: NextPage = () => {
           Create a meow community
         </h2>
 
-        {/* <PostContent post={post} setPost={setPost} errors={errors} /> */}
+        <div className="my-3 flex flex-col gap-2">
+          <input
+            value={community.name}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setCommunity({ ...community, name: event.target.value })
+            }
+            placeholder="Community name"
+            className={`h-fit w-full rounded-md border-neutral-700 bg-neutral-800 p-2 text-gray-200 ${
+              errors?.name ? "border-2 border-red-600" : "border-transparent"
+            }`}
+          />
+          {errors?.name &&
+            errors.name.map((error, idx) => (
+              <p key={idx} className="text-sm font-medium text-red-500">
+                {error}
+              </p>
+            ))}
+          <textarea
+            value={community.description}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setCommunity({ ...community, description: event.target.value })
+            }
+            placeholder="Description"
+            className={`w-full rounded-md border-neutral-700 bg-neutral-800 p-2 text-gray-200 ${
+              errors?.name ? "border-2 border-red-600" : "border-transparent"
+            }`}
+          />
+          {errors?.description &&
+            errors.description.map((error, idx) => (
+              <p key={idx} className="text-sm font-medium text-red-500">
+                {error}
+              </p>
+            ))}
+        </div>
+
         <div className="flex justify-end">
           <button
-            onClick={() =>
-              handleNewCommunityButton({
-                name: post.name,
-                description: post.description,
-              })
-            }
+            onClick={() => handleNewCommunityButton()}
             className="my-3 rounded-xl border-2 bg-gray-300 px-3 py-0.5 text-sm font-[600] hover:bg-gray-100"
           >
             Create
