8f2d0d9a649d893c5ca5ec78955df0a0eeabb036
oasido <asido.ofek@gmail.com>
2022-12-03 03:20:49 +0200

    feat: 🎸 show pulsing skeleton if loading

--- src/components/home-sidebar/community-button.tsx
+++ src/components/home-sidebar/community-button.tsx
@@ -11,13 +11,17 @@ export const CommunityButton = ({
   title,
   name,
 }: CommunityButtonProps) => {
-  return isLoading ? (
-    <div className="mx-2 rounded-xl border-2 bg-transparent px-3 py-0.5 hover:bg-gray-500/10" />
-  ) : (
-    <Link href={`/r/${name}`}>
-      <button className="mx-2 rounded-xl border-2 bg-transparent px-3 py-0.5 text-sm font-[600] text-gray-300 hover:bg-gray-500/10">
-        {title ?? name}
-      </button>
-    </Link>
-  );
+  if (isLoading === true) {
+    return (
+      <div className="mx-2 h-5 animate-pulse rounded-xl bg-gray-300 px-3 py-0.5 dark:bg-gray-400" />
+    );
+  } else {
+    return (
+      <Link href={`/r/${name}`}>
+        <button className="mx-2 rounded-xl border-2 bg-transparent px-3 py-0.5 text-sm font-[600] text-gray-300 hover:bg-gray-500/10">
+          {title ?? name}
+        </button>
+      </Link>
+    );
+  }
 };
